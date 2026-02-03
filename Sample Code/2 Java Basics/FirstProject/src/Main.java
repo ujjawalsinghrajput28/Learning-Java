633e7eb8c9a3a9684d008faf15ef7903eef88d54
ujjawalsinghrajput28 <ujjawalsinghrajput28@gmail.com>
2026-02-03 20:01:59 +0530

    Add files via upload

---
+++ Sample Code/2 Java Basics/FirstProject/src/Main.java
@@ -0,0 +1,7 @@
+package FirstProject.src;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome  Coding");
+        System.out.println("asdfasdf");
+    }
+}
